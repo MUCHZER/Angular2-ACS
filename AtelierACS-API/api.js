8a3dc9223892d1a853418f81f6ce560a8b55185e
Léo G <leograndcontact@gmail.com>
2017-01-05 18:20:58 +0100

    adding cors & test route

--- AtelierACS-API/api.js
+++ AtelierACS-API/api.js
@@ -1,18 +1,42 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
-app.get('/test/:id', function(req, res){
+app.use(cors());
+
+app.get('/button/:id', function(req, res){
+    
     user = {
         1: 'Jean',
         2: 'Francis'
     };
+
     if ( user[ req.params.id ] ) {        
-        res.status(200).send( user[ req.params.id ] );
+        res.status(200).send( { 
+            'res': user[ req.params.id ] 
+        });
     } else {
         res.status(500).send('Something broke!')
     }
+
 });
 
+
+app.post('/test/', function(req, res){
+    
+    user = {
+        1337: 'r3k7'
+    };
+
+    if ( user[ req.params.id ] ) {        
+        res.status(200).send( { 
+            'res': user[ req.params.id ] 
+        });
+    } else {
+        res.status(200).send('C\'est bon!');
+    }
+    
+});
 
 app.listen(8080, function () {
   console.log('API started on port 8080 :)');
